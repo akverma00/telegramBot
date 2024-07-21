b50274e1a3db7da22d895019093ae6b48038138f
Arun Kumar Verma <arun.verma@inmobi.com>
2024-07-21 19:00:15 +0530

    Updated and fixed

--- telegramBot/telegram_bot.py
+++ telegramBot/telegram_bot.py
@@ -10,18 +10,28 @@ load_dotenv()
 TELEGRAM_API_ID = os.getenv('TELEGRAM_API_ID')
 TELEGRAM_API_HASH = os.getenv('TELEGRAM_API_HASH')
 SESSION_NAME = 'session_name'
-TARGET_CHAT_IDS = [
-    -1001945508703,
-    -1001833328420,
-    -1001905014374,
-    -1002121131434,
-    -1002133513866,
-    -1002056442424
-]
-SOURCE_CHAT_IDS = [
-    -1002008905623,
-    -1001809265514
-]
+
+# Dictionary mapping source chat IDs to lists of target chat IDs.
+# Each key in the dictionary is a source chat ID, and 
+# the corresponding value is a list of target chat IDs to which messages from the source chat should be forwarded.
+SOURCE_TO_TARGET_MAP = {
+    -1002008905623: [
+        -1001945508703,
+        -1001833328420,
+        -1001905014374,
+        -1002121131434,
+        -1002133513866,
+        -1002056442424
+    ],
+    -1001809265514: [
+        -1001945508703,
+        -1001833328420,
+        -1001905014374,
+        -1002121131434,
+        -1002133513866,
+        -1002056442424
+    ]
+}
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -31,7 +41,7 @@ logger = logging.getLogger(__name__)
 client = TelegramClient(SESSION_NAME, TELEGRAM_API_ID, TELEGRAM_API_HASH)
 
 async def send_to_groups(event):
-    for chat_id in TARGET_CHAT_IDS:
+    for chat_id in SOURCE_TO_TARGET_MAP.get(event.chat_id):
         try:
             await client.send_message(chat_id, event.message)
             logger.info(f"Message forwarded to chat ID {chat_id}")
@@ -39,17 +49,17 @@ async def send_to_groups(event):
             logger.error(f"Failed to send message to chat ID {chat_id}: {e}")
 
 # Function that ignores messages constains http and @ in the event.message.message
-def isMessageValidated(event):
-    if 'http' in event.message.message or '@' in event.message.message:
+def isMessageValidated(message):
+    if 'http' in message or '@' in message:
         return False
     return True
 
-@client.on(events.NewMessage(chats=SOURCE_CHAT_IDS))
+@client.on(events.NewMessage(chats=list(SOURCE_TO_TARGET_MAP.keys())))
 async def handler(event):
-    if isMessageValidated(event):
+    if isMessageValidated(event.message.message):
         await send_to_groups(event)
     else:
-        logger.info("Message contains http or @, ignored")
+        logger.info(f"Message from {event.chat_id} contains http or @, ignored: {event.message.message}")
 
 def start_bot():
     with client:
